385a2d0a185dc422f81cddd3f2b7c0aa2c406fab
jiajunlong <youximao123@gmail.com>
2017-07-21 21:30:11 +0800

    这是第一次提交

---
+++ lost_found/src/main/java/com/whut/lost_found/LostFoundApplication.java
@@ -0,0 +1,12 @@
+package com.whut.lost_found;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LostFoundApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LostFoundApplication.class, args);
+	}
+}
