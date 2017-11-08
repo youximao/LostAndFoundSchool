7b99dee65ebec7d5243c722d3fd1cb132b806055
jiajunlong <youximao123@gmail.com>
2017-11-08 20:02:32 +0800

    验证码生成

--- lost_found/src/main/java/com/whut/lost_found/web/QrcodeCon.java
+++ lost_found/src/main/java/com/whut/lost_found/web/QrcodeCon.java
@@ -7,8 +7,13 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 import javax.imageio.ImageIO;
 import javax.servlet.http.HttpServletResponse;
+import java.awt.*;
 import java.awt.image.BufferedImage;
-import java.io.*;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.Random;
 
 /**
  * Created by Administrator on 2017/11/7 0007.
@@ -59,5 +64,34 @@ public class QrcodeCon {
         //OutputStream outputStream=new ObjectOutputStream(file);
 
     }
+
+    @RequestMapping("/yanzhengCode")
+    public void getYanZhengCode(HttpServletResponse response){
+        BufferedImage bi=new BufferedImage(68,22,BufferedImage.TYPE_INT_BGR);
+        Graphics g=bi.getGraphics();
+        Color c=new Color(250,150,255);
+        g.setColor(c);
+        g.fillRect(0,0,68,22);
+        //验证码字符集合
+        char[] ch="ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789".toCharArray();
+        Random r=new Random();
+        int len=ch.length;
+        int index;
+        StringBuilder sb=new StringBuilder();
+        for(int i=0;i<4;++i){
+            index=r.nextInt(len);
+            //设置验证码字符随机颜色
+            g.setColor(new Color(r.nextInt(88),r.nextInt(188),r.nextInt(255)));
+            //画出对应随机的验证码字符
+            g.drawString(ch[index]+"",(i*15)+3,18);
+            sb.append(ch[index]);
+        }
+        try {
+            ImageIO.write(bi,"GIF",response.getOutputStream());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
 
 }
--- lost_found/src/main/resources/static/js/Qrcode.js
+++ lost_found/src/main/resources/static/js/Qrcode.js
@@ -11,6 +11,18 @@ function getImg() {
 }
 function flushQr() {
    // alert("ok");
-    $("#imgflu").attr("src","./getone");
+    var src=$("#imgflu").attr("src");
+
+    var shu=100*Math.random();
+    if(src!="./getone") {
+        $("#imgflu").attr("src", "./getone"+"?time"+shu);
+    }else {
+        $("#imgflu").attr("src", "./getflush"+"?time"+shu);
+    }
     console.info("daozhe");
+}
+
+function getYanZhengCode() {
+    var shu=100*Math.random();
+    $("#imgs").attr("src", "./yanzhengCode"+"?time="+shu);
 }
